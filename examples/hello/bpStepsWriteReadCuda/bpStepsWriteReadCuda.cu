73b60e82e677825f5e443a3a684d4d6de28faf49
Franz Pöschel <franz.poeschel@gmail.com>
2024-09-17 18:13:36 +0200

    Add example for GPU memory selection
    Square memselect output
    
    Rectangle instead of square
    
    Just use another test so I don't have to edit the CMake logic
    
    Put the code for this in bpStepsWriteReadCuda.cu

--- examples/hello/bpStepsWriteReadCuda/bpStepsWriteReadCuda.cu
+++ examples/hello/bpStepsWriteReadCuda/bpStepsWriteReadCuda.cu
@@ -1,142 +1,71 @@
-/*
- * Distributed under the OSI-approved Apache License, Version 2.0.  See
- * accompanying file Copyright.txt for details.
- *
- * bpStepsWriteReadCuda.cu  Simple example of writing and reading data through ADIOS2 BP engine with
- * multiple simulations steps for every IO step using CUDA
- */
-
-#include <ios>
-#include <iostream>
-#include <stdexcept> //std::invalid_argument std::exception
-#include <string>
-#include <vector>
+#include "adios2/common/ADIOSTypes.h"
 
 #include <adios2.h>
-
 #include <cuda_runtime.h>
+#include <cuda_runtime_api.h>
+#include <iostream>
 
-__global__ void update_array(float *vect, int val) { vect[blockIdx.x] += val; }
+using value_type = long;
 
-void writer(adios2::ADIOS &adios, const std::string &engine, const std::string &fname,
-            const size_t Nx, unsigned int nSteps)
+__global__ void __cuda_init_array(value_type *devPtr, size_t width, size_t height, size_t pitch)
 {
-    // Initialize the simulation data
-    float *gpuSimData;
-    cudaMalloc(&gpuSimData, Nx * sizeof(float));
-    cudaMemset(gpuSimData, 0, Nx);
-
-    // Set up the ADIOS structures
-    adios2::IO bpIO = adios.DeclareIO("WriteIO");
-    bpIO.SetEngine(engine);
-
-    // Declare an array for the ADIOS data of size (NumOfProcesses * Nx)
-    const adios2::Dims shape{static_cast<size_t>(Nx)};
-    const adios2::Dims start{static_cast<size_t>(0)};
-    const adios2::Dims count{Nx};
-    auto bpFloats = bpIO.DefineVariable<float>("bpFloats", shape, start, count);
-    auto bpStep = bpIO.DefineVariable<unsigned int>("bpStep");
-
-    adios2::Engine bpWriter = bpIO.Open(fname, adios2::Mode::Write);
-
-    // Simulation steps
-    for (unsigned int step = 0; step < nSteps; ++step)
-    {
-        // Make a 1D selection to describe the local dimensions of the
-        // variable we write and its offsets in the global spaces
-        const adios2::Box<adios2::Dims> sel({0}, {Nx});
-        bpFloats.SetSelection(sel);
-
-        // Start IO step every write step
-        bpWriter.BeginStep();
-        bpFloats.SetMemorySpace(adios2::MemorySpace::GPU);
-        bpWriter.Put(bpFloats, gpuSimData);
-        bpWriter.Put(bpStep, step);
-        bpWriter.EndStep();
-
-        // Update values in the simulation data
-        update_array<<<Nx, 1>>>(gpuSimData, 10);
-    }
-
-    bpWriter.Close();
+    size_t idx = sizeof(value_type) * threadIdx.x + threadIdx.y * pitch;
+    auto val = threadIdx.y * width + threadIdx.x;
+    reinterpret_cast<value_type &>((reinterpret_cast<char *>(devPtr))[idx]) =
+        static_cast<value_type>(val);
 }
 
-void reader(adios2::ADIOS &adios, const std::string &engine, const std::string &fname,
-            const size_t Nx, unsigned int /*nSteps*/)
+void cuda_init_array(value_type *devPtr, size_t width, size_t height, size_t pitch)
 {
-    // Create ADIOS structures
-    adios2::IO bpIO = adios.DeclareIO("ReadIO");
-    bpIO.SetEngine(engine);
-
-    adios2::Engine bpReader = bpIO.Open(fname, adios2::Mode::Read);
-
-    unsigned int inStep = 0;
-    float *gpuSimData;
-    cudaMalloc(&gpuSimData, Nx * sizeof(float));
-    cudaMemset(gpuSimData, 0, Nx);
-    for (unsigned int step = 0; bpReader.BeginStep() == adios2::StepStatus::OK; ++step)
-    {
-        auto bpFloats = bpIO.InquireVariable<float>("bpFloats");
-        if (bpFloats)
-        {
-            const adios2::Dims start{0};
-            const adios2::Dims count{Nx};
-            const adios2::Box<adios2::Dims> sel(start, count);
-            bpFloats.SetSelection(sel);
-
-            bpFloats.SetMemorySpace(adios2::MemorySpace::GPU);
-            bpReader.Get(bpFloats, gpuSimData); //, adios2::Mode::Deferred);
-        }
-        auto bpStep = bpIO.InquireVariable<unsigned int>("bpStep");
-        if (bpStep)
-        {
-            bpReader.Get(bpStep, &inStep);
-        }
-
-        bpReader.EndStep();
-        if (inStep != step)
-        {
-            std::cout << "ERROR: step mismatch\n";
-            return;
-        }
-    }
-    bpReader.Close();
+    dim3 threadsPerBlock(width, height);
+    std::cout << "WIDTH:\t" << width << "\tHEIGHT:\t" << height << std::endl;
+    __cuda_init_array<<<1, threadsPerBlock>>>(devPtr, width, height, pitch);
 }
 
-int main(int argc, char **argv)
+auto main() -> int
 {
-    const int device_id = 1;
-    cudaSetDevice(device_id);
+    constexpr size_t width = 10;
+    constexpr size_t height = 20;
 
-    const std::string engine = argv[1] ? argv[1] : "BPFile";
-    std::cout << "Using engine " << engine << std::endl;
+    void *devPtr;
+    size_t pitch;
+    cudaMallocPitch(&devPtr, &pitch, width * sizeof(value_type), height);
+    cuda_init_array(static_cast<value_type *>(devPtr), width, height, pitch);
+    std::cout << "Pitch: " << pitch / sizeof(value_type) << '\n';
 
-    const std::string filename = engine + "StepsWriteReadCuda.bp";
-    const unsigned int nSteps = 10;
-    const unsigned int Nx = 6000;
-    try
-    {
-        /** ADIOS class factory of IO class objects */
-        adios2::ADIOS adios;
+    adios2::ADIOS adios;
+    auto IO = adios.DeclareIO("asdfg");
+    IO.SetEngine("bp5");
+    auto engine = IO.Open("MemSelection.bp", adios2::Mode::Write);
+    engine.BeginStep();
 
-        writer(adios, engine, filename, Nx, nSteps);
-        reader(adios, engine, filename, Nx, nSteps);
-    }
-    catch (std::invalid_argument &e)
     {
-        std::cout << "Invalid argument exception, STOPPING PROGRAM\n";
-        std::cout << e.what() << "\n";
-    }
-    catch (std::ios_base::failure &e)
-    {
-        std::cout << "IO System base failure exception, STOPPING PROGRAM\n";
-        std::cout << e.what() << "\n";
-    }
-    catch (std::exception &e)
-    {
-        std::cout << "Exception, STOPPING PROGRAM\n";
-        std::cout << e.what() << "\n";
+        auto var = IO.DefineVariable<value_type>("gpu_pointer_without_mem_selection",
+                                                 {height, width}, {0, 0}, {height, width});
+        engine.Put(var, static_cast<value_type const *>(devPtr));
     }
 
-    return 0;
+    /*
+     * This is the logic that does not work.
+     */
+    {
+        auto var = IO.DefineVariable<value_type>("gpu_pointer_with_mem_selection", {height, width},
+                                                 {0, 0}, {height, width});
+        var.SetMemorySelection({{0, 0}, {height, pitch / sizeof(value_type)}});
+        engine.Put(var, static_cast<value_type const *>(devPtr));
+    }
+
+    size_t flattened_non_paddened_extent = height * width;
+    std::unique_ptr<value_type[]> hostPtr{new value_type[flattened_non_paddened_extent]};
+    cudaMemcpy2D(hostPtr.get(), sizeof(value_type) * width, devPtr, pitch,
+                 width * sizeof(value_type), height, cudaMemcpyDeviceToHost);
+
+    {
+        auto var = IO.DefineVariable<value_type>("cpu_pointer_without_padding", {width, height},
+                                                 {0, 0}, {width, height});
+        engine.Put(var, hostPtr.get());
+    }
+
+    engine.EndStep();
+    engine.Close();
 }
